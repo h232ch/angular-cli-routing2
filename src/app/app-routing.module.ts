465e93ccfb3729309cb70bd2c211c25c7bab734e
sean <sean@seanui-MacBookPro.local>
2023-02-25 20:46:35 +0900

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,59 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {Injectable, NgModule} from '@angular/core';
+import {ResolveFn, RouterModule, RouterStateSnapshot, Routes, TitleStrategy} from '@angular/router';
+import { FirstComponent } from '../first/first.component';
+import { SecondComponent } from '../second/second.component';
+import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
+import {ChildAComponent} from "../child-a/child-a.component";
+import {ChildBComponent} from "../child-b/child-b.component";
+import {Title} from "@angular/platform-browser";
+import {HeoresComponent} from "../heores/heores.component";
 
-const routes: Routes = [];
+
+@Injectable({providedIn: 'root'})
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`My Application | ${title}`);
+    }
+  }
+}
+
+const resolvedChildATitle: ResolveFn<string> = () => Promise.resolve('child a');
+
+const routes: Routes = [
+  { path: 'first-component',
+    component: FirstComponent,
+    children: [
+      {
+        path: 'child-a',
+        title: resolvedChildATitle,
+        component: ChildAComponent
+      },{
+        path: 'child-b',
+        title: 'child!!!B',
+        component: ChildBComponent
+      }
+    ]},
+  { path: 'heroes', component: HeoresComponent },
+  { path: 'second-component', component: SecondComponent },
+  { path: '**', component: PageNotFoundComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {provide: TitleStrategy, useClass: TemplatePageTitleStrategy},
+  ]
 })
-export class AppRoutingModule { }
+
+
+
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
+import {ChildAComponent} from "../child-a/child-a.component";
+import {ChildBComponent} from "../child-b/child-b.component";
+import {FirstComponent} from "../first/first.component";
+import {SecondComponent} from "../second/second.component";
+import {YourGuardComponent} from "../your-guard/your-guard.component";
+import {HeoresComponent} from "../heores/heores.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    ChildAComponent,
+    ChildBComponent,
+    FirstComponent,
+    SecondComponent,
+    YourGuardComponent,
+    HeoresComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
